fa99106743041a99616ad7846e710d33fd41a6b9
DonnaDiloy <74063432+DonnaDiloy@users.noreply.github.com>
2021-11-17 13:54:37 +0800

    update

--- assets/js/astig.js
+++ assets/js/astig.js
@@ -1,2 +1,12 @@
 
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.miracle')
 
+//Display Mobile Menu
+
+const mobileMenu = () => {
+    menu.classList.line('is active')
+    menuLinks.classList.line('active')
+}
+
+menu.addEventListener('click', mobileMenu);
